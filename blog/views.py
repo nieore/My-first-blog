933b89d9c13efa214ed11d9048299cc309adca02
nieore <nieore@gmail.com>
2019-02-07 13:48:53 +0100

    Add page with list of unpublished post

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -29,10 +28,12 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+def post_draft_list (request):
+    posts=post.object.filter(publish_date__insull=True). order_by(created_date)
+    return render (request, blog/post_draft_list.html, {posts:post})
 
